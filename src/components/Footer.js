ac233338ef8e77aacbd40bdff429859ef521961d
= <=>
2019-05-03 22:52:26 -0500

    Striped Background.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo-02.svg'
+import stripes from '../img/stripe-background.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import youtube from '../img/social/youtube.svg'
@@ -9,14 +10,16 @@ import youtube from '../img/social/youtube.svg'
 const Footer = class extends React.Component {
   render() {
     return (
+      <div>
+      <div className="content has-text-centered">
+        <img
+          src={logo}
+          alt="Weather Bros."
+          style={{ width: '14em', height: '10em' }}
+        />
+      </div>
+      <div className="has-background-black stripes-background"></div>
       <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Weather Bros."
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
@@ -96,6 +99,7 @@ const Footer = class extends React.Component {
           </div>
         </div>
       </footer>
+      </div>
     )
   }
 }
