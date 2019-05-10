0da666031a6dbbcd17a3fc3345325d0e8198dcd2
= <=>
2019-05-10 00:22:31 -0500

    Removed the images.

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -58,27 +58,29 @@ export const ProductPageTemplate = ({
                   <p>{main.description}</p>
                 </div>
               </div>
-              <div className="tile is-ancestor">
-                <div className="tile is-vertical">
-                  <div className="tile">
-                    <div className="tile is-parent is-vertical">
-                      <article className="tile is-child">
-                        <PreviewCompatibleImage imageInfo={main.image1} />
-                      </article>
-                    </div>
-                    <div className="tile is-parent">
-                      <article className="tile is-child">
-                        <PreviewCompatibleImage imageInfo={main.image2} />
-                      </article>
-                    </div>
-                  </div>
-                  <div className="tile is-parent">
-                    <article className="tile is-child">
-                      <PreviewCompatibleImage imageInfo={main.image3} />
-                    </article>
-                  </div>
-                </div>
-              </div>
+              {
+              // <div className="tile is-ancestor">
+              //   <div className="tile is-vertical">
+              //     <div className="tile">
+              //       <div className="tile is-parent is-vertical">
+              //         <article className="tile is-child">
+              //           <PreviewCompatibleImage imageInfo={main.image1} />
+              //         </article>
+              //       </div>
+              //       <div className="tile is-parent">
+              //         <article className="tile is-child">
+              //           <PreviewCompatibleImage imageInfo={main.image2} />
+              //         </article>
+              //       </div>
+              //     </div>
+              //     <div className="tile is-parent">
+              //       <article className="tile is-child">
+              //         <PreviewCompatibleImage imageInfo={main.image3} />
+              //       </article>
+              //     </div>
+              //   </div>
+              // </div>
+              }
               <Testimonials testimonials={testimonials} />
               <div
                 className="full-width-image-container"
