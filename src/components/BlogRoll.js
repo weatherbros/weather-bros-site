6565f90a955373e6bb050bb4174e104ced408aab
= <=>
2019-05-19 22:10:29 -0500

    Featured Images.

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -4,6 +4,8 @@ import { Link, graphql, StaticQuery } from 'gatsby'
 
 class BlogRoll extends React.Component {
   render() {
+    
+
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
@@ -13,6 +15,7 @@ class BlogRoll extends React.Component {
           posts.map(({ node: post }) => (
             <div className="is-parent column is-6" key={post.id}>
               <article className="tile is-child box">
+                <img src={"/img/blog-featured-" + (Math.floor(Math.random() * (4 - 1 + 1) + 1)) + ".jpg"}></img>
                 <p>
                   <Link
                     className="title has-text-primary is-size-4"
